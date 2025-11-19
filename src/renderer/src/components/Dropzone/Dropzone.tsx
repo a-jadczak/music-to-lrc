6fb17824cecce370a5edb667730ede3def1d29d7
Arkadiusz Jadczak <arkadiusz.jadczak7@gmail.com>
2025-11-19 19:40:24 +0100

    Added useCallback hooks in Dropzone

--- src/renderer/src/components/Dropzone/Dropzone.tsx
+++ src/renderer/src/components/Dropzone/Dropzone.tsx
@@ -3,7 +3,7 @@ import Upload from '@mui/icons-material/Upload';
 import { Box, Typography } from '@mui/material';
 import { FilesContext } from '@renderer/contexts/FilesContext';
 import { getFileExtension } from '@renderer/utils/stringUtils';
-import { useContext, useState } from 'react';
+import { useCallback, useContext, useState } from 'react';
 import AudioFile from 'src/types/AudioFile';
 import uniqid from 'uniqid';
 
@@ -11,37 +11,40 @@ const Dropzone = () => {
   const { addFiles } = useContext(FilesContext)!;
   const [dragging, setDragging] = useState(false);
 
-  const uploadFiles = async () => {
-    const dialogResult = await window.electronAPI.uploadFiles();
+  const uploadFiles = useCallback(async () => {
+    const result = await window.electronAPI.uploadFiles();
+    if (!result.canceled) {
+      addFiles(result.files);
+    }
+  }, [addFiles]);
 
-    if (dialogResult.canceled) return;
+  const handleDrop = useCallback(
+    (e: React.DragEvent<HTMLDivElement>) => {
+      e.preventDefault();
+      const files = Array.from(e.dataTransfer?.files || []);
+      addFiles(createAudioFiles(files));
+      setDragging(false);
+    },
+    [addFiles]
+  );
 
-    console.log(dialogResult);
-    addFiles(dialogResult.files);
-  };
-
-  function handleDrop(e: React.DragEvent<HTMLDivElement>) {
-    e.preventDefault();
-    const files = Array.from(e.dataTransfer?.files || []);
-    // For safety purposes, getting a file path is not allowed so we have to create an custom URL to be able to read file
-    const newFiles: AudioFile[] = files.map((file) => ({
+  const createAudioFiles = (files: File[]): AudioFile[] => {
+    return files.map((file) => ({
       id: `file-${uniqid()}`,
       name: file.name,
       size: file.size,
       type: getFileExtension(file.name),
+      // For safety purposes, getting a file path is not allowed so we have to create an custom URL to be able to read file
       path: URL.createObjectURL(file)
     }));
+  };
 
-    console.log(newFiles);
-    addFiles(newFiles);
-    handleDragLeave();
-  }
-
-  function handleDragOver(e: React.DragEvent<HTMLDivElement>) {
+  const handleDragOver = useCallback((e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
     setDragging(true);
-  }
-  const handleDragLeave = () => setDragging(false);
+  }, []);
+
+  const handleDragLeave = useCallback(() => setDragging(false), []);
 
   return (
     <Box className="dropzone-container">
